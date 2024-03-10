335889a72efbc52a06a5d06a45edf2c722bb7a49
Faystos <fayst_@bk.ru>
2024-03-11 01:27:29 +0300

    add left panel page list tasks

--- src/app/layouts/task-list-layout/task.list.component.ts
+++ src/app/layouts/task-list-layout/task.list.component.ts
@@ -1,9 +1,41 @@
-import {ChangeDetectionStrategy, Component} from "@angular/core";
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { shareReplay } from "rxjs";
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+
+import { AppFacadeService } from "../../store/state/app.facade.service";
+import { ITask } from "../../types/task.type";
+
 
 @Component({
-  template: `<span>Тут будет компонент списка задач</span>`,
+  templateUrl: 'task.list.component.html',
+  styleUrls: ['task.list.component.scss'],
   selector: 'task-list-component',
   standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonToggleModule,
+    MatCardModule
+  ]
 })
-export class TaskListComponent {}
+export class TaskListComponent implements OnInit {
+  public tasks$ = this.appFacadeService.taskList$.pipe(shareReplay());
+  private task!: ITask
+
+  constructor(private appFacadeService: AppFacadeService) {}
+
+  ngOnInit() {
+    console.log(this.task)
+  }
+
+  onSelectTask(evt: ITask) {
+    this.task = evt;
+  }
+}
