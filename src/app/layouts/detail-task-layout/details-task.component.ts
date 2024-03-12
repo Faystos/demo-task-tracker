e981c5e5114c44a09707fbfe578c17ac3d128873
Faystos <fayst_@bk.ru>
2024-03-12 15:05:57 +0300

    add button back

--- src/app/layouts/detail-task-layout/details-task.component.ts
+++ src/app/layouts/detail-task-layout/details-task.component.ts
@@ -1,7 +1,10 @@
 import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { ActivatedRoute } from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import { map } from "rxjs";
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { TaskComponent } from "../../shared/components/task/task.component";
 import { AppFacadeService } from "../../store/state/app.facade.service";
@@ -11,11 +14,14 @@ import { ITask } from "../../types/task.type";
 @Component({
   templateUrl: 'details-task.component.html',
   selector: 'details-task-component',
+  styleUrls: ['details-task.component.scss'],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     CommonModule,
-    TaskComponent
+    TaskComponent,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class DetailsTaskComponent implements OnInit {
@@ -28,12 +34,17 @@ export class DetailsTaskComponent implements OnInit {
 
   constructor(
     private appFacadeService: AppFacadeService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
     this.route.params.subscribe(value => {
       this.taskId = `${value['id']}`;
     });
+  }
+
+  onBack() {
+    this.router.navigate(['']);
   }
 }
